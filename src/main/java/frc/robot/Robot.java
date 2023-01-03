d004e5729df0d7dea9d6a8b1ad016bbbe1d8a88e
dracco1993 <dracco1993@gmail.com>
2023-01-03 16:34:39 -0500

    Added basic drivetrain and simulation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,75 +4,84 @@
 
 package frc.robot;
 
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class. If you change the name of this class or the
- * package after creating this project, you must also update the build.gradle file in the project.
- */
-public class Robot extends RobotBase {
-  public void robotInit() {}
+import java.util.List;
 
-  public void disabled() {}
+public class Robot extends TimedRobot {
+  private final XboxController m_controller = new XboxController(0);
 
-  public void autonomous() {}
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_speedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
-  public void teleop() {}
+  private final Drivetrain m_drive = new Drivetrain();
+  private final RamseteController m_ramsete = new RamseteController();
+  private final Timer m_timer = new Timer();
+  private Trajectory m_trajectory;
 
-  public void test() {}
-
-  private volatile boolean m_exit;
+  private final Field2d m_field = new Field2d();
 
   @Override
-  public void startCompetition() {
-    robotInit();
+  public void robotInit() {
+    SmartDashboard.putData("Field", m_field);
 
-    // Tell the DS that the robot is ready to be enabled
-    HAL.observeUserProgramStarting();
-
-    while (!Thread.currentThread().isInterrupted() && !m_exit) {
-      if (isDisabled()) {
-        DriverStation.inDisabled(true);
-        disabled();
-        DriverStation.inDisabled(false);
-        while (isDisabled()) {
-          DriverStation.waitForData();
-        }
-      } else if (isAutonomous()) {
-        DriverStation.inAutonomous(true);
-        autonomous();
-        DriverStation.inAutonomous(false);
-        while (isAutonomousEnabled()) {
-          DriverStation.waitForData();
-        }
-      } else if (isTest()) {
-        LiveWindow.setEnabled(true);
-        Shuffleboard.enableActuatorWidgets();
-        DriverStation.inTest(true);
-        test();
-        DriverStation.inTest(false);
-        while (isTest() && isEnabled()) {
-          DriverStation.waitForData();
-        }
-        LiveWindow.setEnabled(false);
-        Shuffleboard.disableActuatorWidgets();
-      } else {
-        DriverStation.inTeleop(true);
-        teleop();
-        DriverStation.inTeleop(false);
-        while (isTeleopEnabled()) {
-          DriverStation.waitForData();
-        }
-      }
-    }
+    m_trajectory = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(2, 2, new Rotation2d()),
+        List.of(),
+        new Pose2d(6, 4, new Rotation2d()),
+        new TrajectoryConfig(2, 2));
   }
 
   @Override
-  public void endCompetition() {
-    m_exit = true;
+  public void robotPeriodic() {
+    m_drive.periodic();
+  }
+
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+    m_drive.resetOdometry(m_trajectory.getInitialPose());
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    double elapsed = m_timer.get();
+    Trajectory.State reference = m_trajectory.sample(elapsed);
+    ChassisSpeeds speeds = m_ramsete.calculate(m_drive.getPose(), reference);
+    m_drive.drive(speeds.vxMetersPerSecond, speeds.omegaRadiansPerSecond);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    double xSpeed = -m_speedLimiter.calculate(m_controller.getLeftY()) * Drivetrain.kMaxSpeed;
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    double rot = -m_rotLimiter.calculate(m_controller.getRightX()) * Drivetrain.kMaxAngularSpeed;
+    m_drive.drive(xSpeed, rot);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    m_drive.simulationPeriodic();
+    m_field.setRobotPose(m_drive.getPose());
   }
 }
