22dc8e789b363b8a063308f4144652e22248ecf7
dracco1993 <dracco1993@gmail.com>
2023-01-09 19:23:33 -0500

    Fix joystick virtualization

--- src/main/java/frc/robot/controls/controllers/DriverController.java
+++ src/main/java/frc/robot/controls/controllers/DriverController.java
@@ -1,5 +1,7 @@
 package frc.robot.controls.controllers;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 public class DriverController extends FilteredController {
   public DriverController(int port) {
     super(port, false, false);
@@ -15,7 +17,11 @@ public class DriverController extends FilteredController {
   }
 
   public double getTurnAxis() {
-    return this.getFilteredAxis(2);
+    if (RobotBase.isReal()) {
+      return this.getFilteredAxis(4);
+    } else {
+      return this.getFilteredAxis(2);
+    }
   }
 
   // Intake
@@ -43,4 +49,28 @@ public class DriverController extends FilteredController {
   public boolean getWantsLower() {
     return this.getYButton();
   }
+
 }
+
+// a is pivot up (should be Y)
+// b is extend down (should be A)
+// x is extend up (should be B)
+// y is pivot down (should be X)
+// set pivot to brake
+// set extend to brake
+
+// public boolean getWantsExtend() {
+// return this.getBButton();
+// }
+
+// public boolean getWantsRetract() {
+// return this.getAButton();
+// }
+
+// public boolean getWantsRaise() {
+// return this.getYButton();
+// }
+
+// public boolean getWantsLower() {
+// return this.getXButton();
+// }
