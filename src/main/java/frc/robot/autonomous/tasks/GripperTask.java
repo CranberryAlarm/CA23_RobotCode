37562ee6ca21348b1db5e6c39bf4a829ca94eb1d
JessePlays3434 <grahajesworksforlucys@gmail.com>
2023-08-24 20:47:14 -0400

    Create a gripper task
    Co-authored-by: GingerJake <gingerjakey@outlook.com>

---
+++ src/main/java/frc/robot/autonomous/tasks/GripperTask.java
@@ -0,0 +1,33 @@
+package frc.robot.autonomous.tasks;
+
+import frc.robot.subsystems.Intake;
+
+public class GripperTask extends Task {
+  private Intake m_intake = Intake.getInstance();
+  private boolean m_open;
+  private boolean m_finished = false;
+
+  public GripperTask(boolean open) {
+    m_open = open;
+  }
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void update() {
+    if(m_open) {
+      m_intake.open();
+    } else {
+      m_intake.close();
+    }
+    m_finished = true;
+  }
+
+  @Override
+  public boolean isFinished() {
+    return m_finished;
+  }
+}
